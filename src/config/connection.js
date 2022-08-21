90e20a026002bf50c54cfddaaf1415daea04b5fa
Alexandru <T_alexandru87@yahoo.com>
2022-08-21 16:22:35 +0100

    connection ready waiting for graphql set-up

---
+++ src/config/connection.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+  try {
+    const MONGODB_URI =
+      process.env.MONGODB_URI || `mongodb://localhost:27017/${process.env.DB_NAME}`;
+
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    await mongoose.connect(MONGODB_URI, options);
+
+    console.log(`[INFO]: Successfully connected to database | ${process.env.DB_NAME}`);
+  } catch (error) {
+    console.log(`[ERROR]: Failed to connect to database | ${error.message}`);
+
+    throw new Error("Failed to connect to database");
+  }
+};
+
+module.exports = connectToDatabase;
