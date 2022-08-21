53867ed409ffb8354bc264e7677657b869ecd039
roxywasiak <ruksclone@hotmail.com>
2022-08-21 16:56:24 +0100

    bug

--- src/models/Ad.js
+++ src/models/Ad.js
@@ -1,28 +1,22 @@
-const {Schema} = require("mongoose");
-// see which model to bring in 
+const { Schema } = require("mongoose");
+// see which model to bring in
 
 const adSchema = {
-    event: {
-    type:Schema.Types.ObjectId,
-    ref:"Event",
-
-    }
-    isPaid: {
-   type: string,
-   required: true,
-   type:Boolean,
-
-
-
-    }
-    expires: {
-   type: Date,
-   required: true,
-   trim: true,
-
-
-    }
-}
+  event: {
+    type: Schema.Types.ObjectId,
+    ref: "Event",
+  },
+  isPaid: {
+    type: string,
+    required: true,
+    type: Boolean,
+  },
+  expires: {
+    type: Date,
+    required: true,
+    trim: true,
+  },
+};
 const schema = new Schema(adSchema);
 const Ad = model("Ad", Schema);
 module.exports = Ad;
