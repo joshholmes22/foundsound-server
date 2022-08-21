e6a886d05351101c393dbc6f07a5c38374f52d9b
Tanveer Bassi <tanveer13a551@gmail.com>
2022-08-21 17:30:40 +0100

    venue JS

--- src/models/Venue.js
+++ src/models/Venue.js
@@ -1,8 +1,18 @@
-const { Schema } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const venueSchema = {
-    address:{},
-    
+  address: {
+    type: string,
+    required: true,
+  },
+  capacity: {
+    type: string,
+    required: true,
+  },
+  facilities: {
+    type: String,
+    enum: ["hasFood", "isAcessibile", "hasCurfew", h"asAlcoholLicense", "hasDressingRooms", "hasSmokingArea", "hasSeating", "isDogFriendly", "hasCloakRoom", "hasShoweringFacilities", "hasToilets", "hygeineRating"],
+  },
 };
 
 const schema = new Schema(venueSchema);
