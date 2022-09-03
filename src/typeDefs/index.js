ad2936f6d9db30ea0661216273baa4ec3460570a
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-09-03 11:32:45 +0100

    fix bug

--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -20,6 +20,25 @@ const typeDefs = gql`
     country: String
     fullAddress: String
   }
+  input AddressInput {
+    _id: ID!
+    formatted_address: [String]
+    thoroughfare: String
+    building_name: String
+    sub_building_name: String
+    sub_building_number: String
+    building_number: String
+    line_1: String
+    line_2: String
+    line_3: String
+    line_4: String
+    locality: String
+    town_or_city: String
+    county: String
+    district: String
+    country: String
+    fullAddress: String
+  }
   type AddressResponse {
     postcode: String
     latitude: String
@@ -65,7 +84,7 @@ const typeDefs = gql`
   }
 
   input VenueInput {
-    address: Address!
+    address: AddressInput!
     capacity: [String]
     facilities: String!
   }
