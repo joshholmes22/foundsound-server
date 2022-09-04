bcfb0e6aad0f40738cf669723b0fc12e57467884
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-09-04 20:04:15 +0100

    console logs removed

--- src/resolvers/createVenue.js
+++ src/resolvers/createVenue.js
@@ -3,21 +3,19 @@ const { Venue, AddressLookup, User, EventOrganiser } = require("../models");
 
 const createVenue = async (_, { venueInput }) => {
   try {
-    console.log(venueInput);
     const { address, capacity, facilities, userType, _id: id } = venueInput;
 
     if (!address || !capacity || !facilities) {
       throw new ApolloError("All required fields are not provided!");
     }
-    console.log(userType);
+
     if (userType === "eventOrganiser") {
       const venue = await Venue.create({ address, capacity, facilities });
-      console.log(venue);
+
       await EventOrganiser.findOneAndUpdate(
         { user: id },
         {
           $push: {
-            // ?? remember to check if it is correct?
             venue: { ...venue },
           },
         }
