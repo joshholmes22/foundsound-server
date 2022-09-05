fc1d81d939cb43e82f3468e99e95b288ae460ba8
roxywasiak <ruksclone@hotmail.com>
2022-09-05 19:45:00 +0100

    added delete event resolver

---
+++ src/resolvers/deleteEvent.js
@@ -0,0 +1,20 @@
+const { ApolloError, AuthenticationError } = require("apollo-server");
+const { Venue, AddressLookup, User, EventOrganiser } = require("../models");
+
+const deleteEvent = async (_, { input }, { user }) => {
+  try {
+    if (!user) {
+      throw new AuthenticationError(
+        "You must be logged in to delete an event ."
+      );
+    }
+    const deletedItem = await Item.findByIdAndDelete(EventId);
+
+    return deletedItem;
+  } catch (error) {
+    console.log(`[ERROR]: Failed to delete event | ${error.message}`);
+    throw new ApolloError("Failed to delete event");
+  }
+};
+
+module.exports = deleteEvent;
