1b1e583c9adb6fd475d3d54bb2b25ab462538da2
roxywasiak <ruksclone@hotmail.com>
2022-09-07 21:12:10 +0100

    readded createevents

--- src/resolvers/createEvent.js
+++ src/resolvers/createEvent.js
@@ -4,31 +4,7 @@ const { Event } = require("../models");
 
 const createEvent = async (_, { createEventInput }) => {
   try {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    if (!input) {
-      throw new ApolloError("All required fields are not provided!");
-    }
-
-    if (input) {
-      const event = await Event.create({
-        ...eventInput,
-      });
-
-      return { event };
-    }
-  } catch (error) {
-    console.log(`[ERROR]: Failed to create venue | ${error.message}`);
-
-    throw new ApolloError("Failed to create venue");
-=======
-    console.log(eventInput);
-    if (!eventInput) {
-      throw new ApolloError("All required fields are not provided!");
-    }
-=======
     const newEvent = await Event.create(createEventInput);
->>>>>>> 9c4658893733b8e59c599c3d055dfeabb3b69356
 
     const event = await Event.findById(newEvent.get("_id"))
       .populate({
@@ -44,7 +20,6 @@ const createEvent = async (_, { createEventInput }) => {
   } catch (error) {
     console.log(`[ERROR]: Failed to create event | ${error.message}`);
     throw new ApolloError("Failed to create event");
->>>>>>> b7a56649ac6d0633cece488ddc80d5193c9a1394
   }
 };
 
