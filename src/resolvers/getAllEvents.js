1b13877d978d8efcdda7c448b9b08de48d599258
roxywasiak <ruksclone@hotmail.com>
2022-09-08 19:45:01 +0100

    finished getallevents

--- src/resolvers/getAllEvents.js
+++ src/resolvers/getAllEvents.js
@@ -1,13 +1,32 @@
 const { ApolloError, AuthenticationError } = require("apollo-server");
-const { Event } = require("../models/Event");
+const Event = require("../models/Event");
 
 const getAllEvents = async (_, { input }) => {
   try {
-    const allEvents = await Event.findAll({});
+    const allEvents = await Event.find({});
     if (allEvents) {
-      return { success: true, allEvents };
+      //return { success: true, allEvents };
+      // let abc = [
+      //   {
+      //     id: "123123",
+      //     name: "abc",
+      //     description: "abc",
+      //     startDate: "abc",
+      //     endDate: "abc",
+      //     time: "abc",
+      //     venue: { address: {} },
+      //     imageUrl: "abc",
+      //     postcode: "abc",
+      //     tags: ["abcd"],
+      //     price: 2.15,
+      //   },
+      // ];
+      if (allEvents.length === 0) {
+        return null;
+      }
+      return allEvents;
     } else {
-      throw new AuthenticationError("No events");
+      throw new ApolloError("No events");
     }
   } catch (error) {
     console.log(`[ERROR]: Failed to find events | ${error.message}`);
