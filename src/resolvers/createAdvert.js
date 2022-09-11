b733f45ad836dbee598e00add69b07a28cf34253
Alexandru <T_alexandru87@yahoo.com>
2022-09-11 16:33:46 +0100

    completed protecting create event, create add and delete event

--- src/resolvers/createAdvert.js
+++ src/resolvers/createAdvert.js
@@ -1,17 +1,23 @@
 const { ApolloError } = require("apollo-server");
 const { Event } = require("../models");
 
-const createAdvert = async (_, { createAdvertInput }) => {
+const createAdvert = async (_, { createAdvertInput }, { user }) => {
   try {
-    const findAd = await Event.findByIdAndUpdate(
-      createAdvertInput.event,
-      {
-        $push: { adverts: createAdvertInput },
-      },
-      { new: true }
-    );
+    const loggedUser = await User.findById(user.id);
 
-    return findAd;
+    if (loggedUser && loggedUser.userType === "eventOrganiser") {
+      const findAd = await Event.findByIdAndUpdate(
+        createAdvertInput.event,
+        {
+          $push: { adverts: createAdvertInput },
+        },
+        { new: true }
+      );
+
+      return findAd;
+    } else {
+      throw new ApolloError("Failed to failed to authenticate user for create advert");
+    }
   } catch (error) {
     console.log(`[ERROR]: Failed to create advert | ${error.message}`);
     throw new ApolloError("Failed to create advert");
--- src/resolvers/deleteEvent.js
+++ src/resolvers/deleteEvent.js
@@ -1,19 +1,25 @@
 const { ApolloError, AuthenticationError } = require("apollo-server");
-const { Event } = require("../models");
+const { Event, User } = require("../models");
 
-const deleteEvent = async (_, { input }) => {
+const deleteEvent = async (_, { input }, { user }) => {
   try {
-    if (!input) {
-      throw new AuthenticationError(
-        "You must be logged in to delete an event ."
-      );
+    const loggedUser = await User.findById(user.id);
+
+    const getEvent = Event.findById({ _id: id });
+
+    if (loggedUser && getEvent.eventOwner === user.id) {
+      if (!input) {
+        throw new AuthenticationError("You must be logged in to delete an event .");
+      }
+
+      const { id } = input;
+      console.log(id);
+      const deletedItem = await Event.findByIdAndDelete({ _id: id });
+
+      return { success: true, deletedItem };
+    } else {
+      throw new ApolloError("Failed to authenticate user for delete process");
     }
-
-    const { id } = input;
-    console.log(id);
-    const deletedItem = await Event.findByIdAndDelete({ _id: id });
-
-    return { success: true, deletedItem };
   } catch (error) {
     console.log(`[ERROR]: Failed to delete event | ${error.message}`);
     throw new ApolloError("Failed to delete event");
