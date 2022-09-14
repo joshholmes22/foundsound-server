716fa56f9f3fff106b0573a3cd90b8e95449bad9
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-09-14 21:15:02 +0100

    createAdvert added

--- src/resolvers/getAllAdsforEventOwner.js
+++ src/resolvers/getAllAdsforEventOwner.js
@@ -1,8 +1,35 @@
-const getAllAdsForEventOwner = async (_, _, { user }) => {
+const { ApolloError } = require("apollo-server");
+const { EventOrganiser } = require("../models");
+
+const getAllAdsForEventOwner = async (_, __, { user }) => {
   try {
+    let filterAds = [];
+    const getAllAds = await Event.find({ eventOwner: user._id })
+      .populate("eventOwner")
+      .populate({
+        path: "adverts",
+        populate: { path: "event", model: "Event" },
+      })
+      .populate({
+        path: "adverts",
+        populate: {
+          path: "event",
+          populate: { path: "eventOwner", model: "User" },
+        },
+      });
+
+    getAllAds.forEach(({ adverts }) => {
+      filterAds = [...filterAds, ...adverts];
+    });
+
+    console.log(filterAds);
+
+    return filterAds;
   } catch (error) {
-    con;
+    console.log(`[ERROR]: Failed to get all adverts | ${error.message}`);
+
+    throw new ApolloError("Failed to get all adverts");
   }
 };
 
-modeule.exports = getAllAdsForEventOwner;
+module.exports = getAllAdsForEventOwner;
